00f2b2b4526462759f3ec2aa077bafd3be8bdb5e
Martin Wendt <github@wwwendt.de>
2023-10-09 21:44:55 +0200

    Use eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+/* eslint-env node */
+module.exports = {
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+  rules: {
+    curly: ["warn", "all"],
+    "prefer-const": "warn",
+    "prefer-spread": "warn",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unused-vars": "warn",
+    // "@typescript-eslint/curly": "warn",
+  },
+};
