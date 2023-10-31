5b014da5f425ac5d845d6f7b0c2f2a486cdea598
Martin Wendt <mar10@wwwendt.de>
2023-10-31 14:28:10 +0100

    Prettier

--- src/wunderbaum.ts
+++ src/wunderbaum.ts
@@ -1895,7 +1895,7 @@ export class Wunderbaum {
     // options = Object.assign({ updateRows: true, renderMarkup: false }, options);
     const defaultMinWidth = 4;
     const vpWidth = this.element.clientWidth;
-    // Shorten last column width to avoid h-scrollbar 
+    // Shorten last column width to avoid h-scrollbar
     // (otherwise resizbing the demo would display a void scrollbar?)
     const FIX_ADJUST_LAST_COL = 1;
     const columns = this.columns;
