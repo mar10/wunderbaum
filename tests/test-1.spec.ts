69d40d86c68e06ed18f5d5593318dc9791af836d
Martin Wendt <github@wwwendt.de>
2023-11-09 14:09:00 +0100

    First test

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,5 +1,12 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('test', async ({ page }) => {
-  // Recording...
+test("has title", async ({ page }) => {
+  await page.goto("http://127.0.0.1:3000/test/unit/test-dev.html");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/✔ Test Suite (DEV) | Wunderbaum/);
 });
+
+// test("test", async ({ page }) => {
+//   // Recording...
+// });
